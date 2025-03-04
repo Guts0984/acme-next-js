61bd11910b61f10985a66ba03476c0327d154c14
Guts0984 <mediumrare1298@gmail.com>
2025-03-04 20:06:54 +0200

    lazy auth implement

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import { NextResponse, NextRequest } from "next/server";
 
-export default NextAuth(authConfig).auth;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
