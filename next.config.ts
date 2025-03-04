465b4e7ff23b57a7a54c901830e96a0f3c49f363
Guts0984 <mediumrare1298@gmail.com>
2025-03-04 19:49:06 +0200

    delete auth

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
   },
+  async redirects() {
+    return [
+      {
+        source: "/", // Redirect from root
+        destination: "/dashboard", // Redirect to /dashboard
+        permanent: true, // Permanent redirect (308)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
